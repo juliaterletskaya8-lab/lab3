93eb5b8073811d624fb87a70be3d12abaa616cb5
juliaterletskaya8-lab <juliaterletskaya8@gmail.com>
2025-09-29 16:07:33 +0000

    Исправлен тест accuracy

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,7 +1,8 @@
+import pytest
 from src.metrics import accuracy_score_custom
 
 def test_accuracy():
     y_true = [1, 0, 1, 1]
     y_pred = [1, 0, 1, 0]
     acc = accuracy_score_custom(y_true, y_pred)
-    assert acc == 0.75
+    assert acc == pytest.approx(0.75, rel=1e-6)
